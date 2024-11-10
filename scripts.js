50d1e6ed317e0bd527c025204cfc7fe4f8252269
ShizaBajwa <119728788+ShizaBajwa@users.noreply.github.com>
2024-11-10 16:54:08 +0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,10 +1,8 @@
-// Select the sections to toggle
 var skillsSection = document.getElementById("skills");
 var workExperienceSection = document.getElementById("work-experience");
-// Select the buttons
 var toggleSkillsButton = document.getElementById("toggle-skills");
 var toggleWorkButton = document.getElementById("toggle-work");
-// Function to toggle the visibility of a section
+
 function toggleVisibility(element) {
     if (element.style.display === "none") {
         element.style.display = "block";
@@ -13,6 +11,5 @@ function toggleVisibility(element) {
         element.style.display = "none";
     }
 }
-// Event listeners for buttons
 toggleSkillsButton.addEventListener("click", function () { return toggleVisibility(skillsSection); });
 toggleWorkButton.addEventListener("click", function () { return toggleVisibility(workExperienceSection); });
